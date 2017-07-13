cb29662fcc97183a913a08d0d8c68c0379640c6c
RadoslawDryzner <radoslaw.dryzner@epfl.ch>
2017-07-13 13:20:59 +0200

    Rendu le code plus robuste en vérifiant l'existance du code html sur une page

--- filter.py
+++ filter.py
@@ -3,9 +3,8 @@ import re
 
 from bs4 import BeautifulSoup
 
-TEST_URL = 'http://test-web-wordpress.epfl.ch'
+TEST_URL = 'test-web-wordpress.epfl.ch'
 SECTIONS_TO_REMOVE = ['recent-comments-2', 'archives-2', 'categories-2', 'meta-2', 'search-2']
-CSS_TO_MODIFY = ['style.css', 'stylisticss.grid.css']
 
 class Filter:
     def response(self, flow):
@@ -14,7 +13,7 @@ class Filter:
         if url[len(url) - 1] == '/' or url[len(url)-5:] == '.html' or url[len(url)-4:] == '.jsp':
             html = BeautifulSoup(flow.response.content, 'html.parser')
             # Modifications apportées aux nouvelles versions du site
-            if url[:len(TEST_URL)] == TEST_URL:
+            if TEST_URL in url and html is not None:
                 # Enlever la barre additionelle inutile des réseaux sociaux
                 for div in html.findAll('div', {'class' : 'addtoany_share_save_container addtoany_content_top'}):
                     div.extract()
@@ -22,13 +21,14 @@ class Filter:
                 # Retrier les elements dans la barre de droite
                 aside = html.find('aside' , {'id' : 'secondary'})
                 toSort = {}
-                for section in aside.findAll('section'):
-                    sectionId = section['id']
-                    if sectionId[:len(sectionId) - 2] == 'black-studio-tinymce':
-                        toSort[sectionId] = section.extract()
-                sortedSections = list(map(lambda x : x[1], sorted(toSort.items())))
-                for section in sortedSections:
-                    aside.append(section)
+                if aside is not None:
+                    for section in aside.findAll('section'):
+                        sectionId = section['id']
+                        if sectionId[:len(sectionId) - 2] == 'black-studio-tinymce':
+                            toSort[sectionId] = section.extract()
+                    sortedSections = list(map(lambda x : x[1], sorted(toSort.items())))
+                    for section in sortedSections:
+                        aside.append(section)
 
                 # Enlever la barre de droite de Wordpress
                 for section in html.findAll('section'):
@@ -38,7 +38,7 @@ class Filter:
                 # Supprimer le footer du site
                 for footer in html.findAll('footer', {'id' : 'colophon'}):
                     footer.extract()
-                        
+
             # Modifications apportées aux sites originaux
             else:
                 # Supprimer le footer du site
